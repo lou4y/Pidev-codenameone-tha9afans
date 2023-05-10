c7e7b879fcf93a03a62bf04824732f8372e98822
Mohamed Lassoued <113787947+med-las@users.noreply.github.com>
2023-05-10 11:41:08 +0100

    Update HomeForm.java

--- src/tha9afans/gui/HomeForm.java
+++ src/tha9afans/gui/HomeForm.java
@@ -26,6 +26,7 @@ public class HomeForm extends Form{
         Button showpanierBtn = new Button("Show Panier");
         Button showFactureBtn = new Button("Show Facture");
         Button signupUser=new Button("signUp");
+        Button quizBtn = new Button ("Quiz");
 
 
 
@@ -67,8 +68,12 @@ public class HomeForm extends Form{
             new LoginForm().show();
         });
         
+        quizBtn.addActionListener((evt) -> {
+            new QuizHomeForm().show();
+        });
+        
         //end
-        this.addAll(addEventBtn,addSessionBtn, showTaskBtn, showpanierBtn,signupUser,showFactureBtn);
+        this.addAll(addEventBtn,addSessionBtn, showTaskBtn, showpanierBtn,signupUser,showFactureBtn, quizBtn);
         
     }
     
